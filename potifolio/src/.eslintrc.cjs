93ab533f833fb4575ced23fa6d4d468fa3af4563
keynpro <sammy-manzi@SAMMY--MANZI.SAMMY-MANZI>
2025-06-26 22:22:17 +0200

    Initial commit

---
+++ potifolio/src/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: [
+        'react',
+    ],
+    rules: {
+        // Add custom rules here
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
---
+++ potifolio/src/Components/poti.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Poti() {
+  return (
+    <div>poti</div>
+  );
+}
+
+export default Poti
